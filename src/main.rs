355c655897d3d9c6131f5209b7dbb5138faf6c25
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2024-06-03 14:25:13 +1000

    Todos

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,15 @@ mod variables;
 // - [ ] Publish v0.1.0
 
 // Ideas:
-// - Prerequisites: Specify a list of applications that must be installed before running a command, or a custom script that must succeed
+// - Preconditions: Specify a list of applications that must be installed before running a command, or a custom script that must succeed
 // - Named actions: Actions can be named so that they can be skipped selectively
 // - Command invocation action: Have an action that invokes another command (Or named action ^), can specify the args
 // - Deferred actions: Always executes at the end, even if one of the actions fails
 // - Include other gecko files (on disk or with a GitHub link)
 // - Pipe config file: example.yaml | gecko do something
 // - Aliases: Have a command alias another command (E.g: gecko deps = docker compose -f deps.yaml). remaining args are passed to the child command
+// - Platform-specific commands.
+// - Cached variable results: Allow the results of an execution variable to be cached for future use.
 // - Remote commands: Execute commands on a remote machine (Like a mini Ansible)
 
 fn main() {
