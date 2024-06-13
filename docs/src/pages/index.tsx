34c85403f67b0206eba28f04ccaea5539dc6db85
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2024-06-13 12:32:48 +1000

    Broken link

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,7 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            /*to="/docs"*/
+            >
             Get Started
           </Link>
         </div>
